edfac949456b3eb8c85710e5ec654e30830a5480
mitchman54 <mitchman54@OBAMA>
2019-03-25 12:16:46 -0500

    Lab 22 Done

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,58 @@
+﻿/* TrieWithNoChildren.cs
+ * Author: Mitchell Moorehead
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// sets a private boolean field to start with empty string
+        /// </summary>
+        private bool _hasEmptyString = false;
+        /// <summary>
+        /// Adds an ITrie
+        /// </summary>
+        /// <param name="s"> string added </param>
+        /// <returns> either a trie with no children or a new trie with one child </returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+                return this;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            return new TrieWithOneChild(s, _hasEmptyString);
+            
+        }
+        /// <summary>
+        /// Checking if a Trie contains a given string
+        /// </summary>
+        /// <param name="s"> the string were looking for </param>
+        /// <returns> whether the trie is empty or not </returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,104 @@
+﻿/* TrieWithOneChild.cs
+ * Author: Mitchell Moorehead
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// Class for a trie containing one child
+    /// </summary>
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool _empty;
+        /// <summary>
+        /// field for 
+        /// </summary>
+        private ITrie _onlyChild;
+        /// <summary>
+        /// the label of the child
+        /// </summary>
+        private char _label;
+        /// <summary>
+        /// Constructor for a trie with one child
+        /// </summary>
+        /// <param name="s"> the string in the trie </param>
+        /// <param name="hasEmpty"> the result of looking if s is an empty string </param>
+        public TrieWithOneChild(string s, bool hasEmpty)
+        {
+            if (s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+
+                throw new ArgumentException();
+            }
+            //_hasEmptyString = hasEmpty;
+            //_children[childLabel - 'a'] = child;
+            //Add(s);
+            _empty = hasEmpty;
+            _label = s[0];
+            _onlyChild = new TrieWithNoChildren().Add(s.Substring(1));
+            
+        }
+        /// <summary>
+        /// Adds a string to a trie
+        /// </summary>
+        /// <param name="s"> string to be added </param>
+        /// <returns> either an added trie, or a new trie with many children </returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _empty = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+
+                if (_label == s[0])
+                {
+                    _onlyChild = _onlyChild.Add(s.Substring(1));
+                    return this;
+                }
+                else
+                {
+                    return new TrieWithManyChildren(s, _empty, _label, _onlyChild);
+                }
+            }
+            return this;
+        }
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s"> the trie to look up</param>
+        /// <returns> the result of the lookup </returns>
+        public bool Contains(string s)
+        {
+
+            if (s == "")
+            {
+                return _empty;
+            }
+            else
+            {
+                if (_label != s[0])
+                {
+                    return false;
+                }
+                else
+                {
+                    return _onlyChild.Contains(s.Substring(1));
+                }
+            }
+        }
+    }
+}
